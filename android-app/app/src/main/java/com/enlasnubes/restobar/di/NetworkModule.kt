ca96c70a1c114318069b575f3e7f9d7a002dbd14
yagocorrales <sanyagouran@gmail.com>
2026-02-20 22:37:10 +0100

    fix: anadir adaptadores Gson para LocalDate/LocalTime/Instant y reducir memoria Gradle

--- android-app/app/src/main/java/com/enlasnubes/restobar/di/NetworkModule.kt
+++ android-app/app/src/main/java/com/enlasnubes/restobar/di/NetworkModule.kt
@@ -4,6 +4,10 @@ import com.enlasnubes.restobar.BuildConfig
 import com.enlasnubes.restobar.data.remote.RestobarApi
 import com.enlasnubes.restobar.data.websocket.WebSocketService
 import com.enlasnubes.restobar.data.websocket.WebSocketManager
+import com.enlasnubes.restobar.data.repository.AuthRepository
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Lazy
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,6 +16,10 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Singleton
 
 @Module
@@ -20,7 +28,44 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideOkHttpClient(authRepository: com.enlasnubes.restobar.data.repository.AuthRepository): OkHttpClient {
+    fun provideGson(): Gson {
+        return GsonBuilder()
+            .registerTypeAdapter(LocalDate::class.java, object : com.google.gson.JsonDeserializer<LocalDate> {
+                override fun deserialize(json: com.google.gson.JsonElement, type: java.lang.reflect.Type, context: com.google.gson.JsonDeserializationContext): LocalDate {
+                    return LocalDate.parse(json.asString)
+                }
+            })
+            .registerTypeAdapter(LocalDate::class.java, object : com.google.gson.JsonSerializer<LocalDate> {
+                override fun serialize(src: LocalDate, typeOfSrc: java.lang.reflect.Type, context: com.google.gson.JsonSerializationContext): com.google.gson.JsonElement {
+                    return com.google.gson.JsonPrimitive(src.format(DateTimeFormatter.ISO_LOCAL_DATE))
+                }
+            })
+            .registerTypeAdapter(LocalTime::class.java, object : com.google.gson.JsonDeserializer<LocalTime> {
+                override fun deserialize(json: com.google.gson.JsonElement, type: java.lang.reflect.Type, context: com.google.gson.JsonDeserializationContext): LocalTime {
+                    return LocalTime.parse(json.asString)
+                }
+            })
+            .registerTypeAdapter(LocalTime::class.java, object : com.google.gson.JsonSerializer<LocalTime> {
+                override fun serialize(src: LocalTime, typeOfSrc: java.lang.reflect.Type, context: com.google.gson.JsonSerializationContext): com.google.gson.JsonElement {
+                    return com.google.gson.JsonPrimitive(src.format(DateTimeFormatter.ISO_LOCAL_TIME))
+                }
+            })
+            .registerTypeAdapter(Instant::class.java, object : com.google.gson.JsonDeserializer<Instant> {
+                override fun deserialize(json: com.google.gson.JsonElement, type: java.lang.reflect.Type, context: com.google.gson.JsonDeserializationContext): Instant {
+                    return Instant.parse(json.asString)
+                }
+            })
+            .registerTypeAdapter(Instant::class.java, object : com.google.gson.JsonSerializer<Instant> {
+                override fun serialize(src: Instant, typeOfSrc: java.lang.reflect.Type, context: com.google.gson.JsonSerializationContext): com.google.gson.JsonElement {
+                    return com.google.gson.JsonPrimitive(src.toString())
+                }
+            })
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(authRepository: Lazy<AuthRepository>): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().apply {
                 level = if (BuildConfig.DEBUG) {
@@ -30,7 +75,7 @@ object NetworkModule {
                 }
             })
             .addInterceptor { chain ->
-                val token = authRepository.getAccessTokenBlocking()
+                val token = authRepository.get().getAccessTokenBlocking()
                 val requestBuilder = chain.request().newBuilder()
                 if (!token.isNullOrBlank()) {
                     requestBuilder.addHeader("Authorization", "Bearer $token")
@@ -42,11 +87,11 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    fun provideRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.API_BASE_URL)
             .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
 
