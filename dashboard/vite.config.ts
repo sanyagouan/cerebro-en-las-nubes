38c19445b0e16ee2e16defc5491638f4c9a820e8
yagocorrales <sanyagouran@gmail.com>
2026-03-01 13:45:27 +0100

    fix(dashboard): Añadido postcss para Tailwind y proxy a prod

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://go84sgscs4ckcs08wog84o0o.app.generaia.site',
         changeOrigin: true,
       },
     },
